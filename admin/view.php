6b661d4dc61792e1c6fe623350eafd8bead27aa3
Akhiljoji145 <akhiljoji1451@gmail.com>
2022-11-06 09:19:00 +0530

    fix

---
+++ admin/view.php
@@ -0,0 +1,17 @@
+<?php
+echo"<html>
+    <head>
+        <link rel="stylesheet" href="../css/view.css">
+    </head>
+    <body>
+    <div class="border">
+        <div class="padding-left">
+        <form>
+        <label>lot no:</label><input type="text" name="lot no">
+        <span><input type="submit" value="search"></span>
+        </form>
+        </div>
+    </div>
+    </body>";
+</html>
+?>
