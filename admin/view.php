6d0dd7d12dc5c8f92540423e11ad78c9297df820
Akhiljoji145 <akhiljoji1451@gmail.com>
2022-11-06 09:21:03 +0530

    fix

--- admin/view.php
+++ admin/view.php
@@ -1,14 +1,14 @@
 <?php
 echo"<html>
     <head>
-        <link rel="stylesheet" href="../css/view.css">
+        <link rel='stylesheet' href='../css/view.css'>
     </head>
     <body>
-    <div class="border">
-        <div class="padding-left">
+    <div class='border'>
+        <div class='padding-left'>
         <form>
-        <label>lot no:</label><input type="text" name="lot no">
-        <span><input type="submit" value="search"></span>
+        <label>lot no:</label><input type='text' name='lot no'>
+        <span><input type='submit' value='search'></span>
         </form>
         </div>
     </div>
