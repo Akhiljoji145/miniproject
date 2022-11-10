aa1067037278858073e21aec27ee9b603d1ebe28
Akhiljoji145 <akhiljoji1451@gmail.com>
2022-11-11 05:24:13 +0530

    fix

--- php/addvehicle.php
+++ php/addvehicle.php
@@ -1,18 +1,17 @@
 <?php
 include("connection.php");
-$vehi_name=$_POST['vehi_name'];
+$vehi_name = $_POST['vehi_name'];
 $vehi_type=$_POST['vehi_type'];
 $vehi_no=$_POST['vehi_no'];
-$owner=$_POST['name'];
-$contact_no=$_POST['no'];
-$query="insert into vehicles values ('','$vehi_name','$vehi_type','$vehi_no','$owner','$contact_no')";
-$result=mysqli_query($conn,$query);
-if($result==TRUE)
+$cust_id=$_POST['id'];
+$phno=$_POST['phno'];
+$arrtime=$_POST['time'];
+$sql="insert into users values('','$vehi_name',$vehi_type','$vehi_no','$cust_id','$phno','$arrtime')";
+$query=mysqli_query($sql);
+if($query==TRUE)
 {
     echo"inserted successfully";
 }
 else
-echo"not inserted";
+echo"not inserted successfully";
 ?>
-
-
