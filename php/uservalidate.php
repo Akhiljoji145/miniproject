c466b674aecb14940570bef2fceee05fe8c1bfbc
Akhiljoji145 <akhiljoji1451@gmail.com>
2022-11-10 22:05:23 +0530

    fix

--- php/uservalidate.php
+++ php/uservalidate.php
@@ -1,1 +1,17 @@
 <?php
+include("connection.php");
+$vehi_name = $_POST['vehi_name'];
+$vehi_type=$_POST['vehi_type'];
+$vehi_no=$_POST['vehi_no'];
+$cust_id=$_POST['id'];
+$phno=$_POST['phno'];
+$arrtime=$_POST['time'];
+$sql="insert into users values('','$vehi_name',$vehi_type','$vehi_no','$cust_id','$phno','$arrtime')";
+$query=mysqli_query($sql);
+if($query==TRUE)
+{
+    echo"inserted successfully";
+}
+else
+echo"not inserted successfully";
+?>
